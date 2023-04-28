4f57f7d65a2fd07404b0048fe86144e0541ef0f0
lnzx <lnx@tutamail.com>
2023-04-28 19:32:37 +0800

    fix sql

--- db.sql
+++ db.sql
@@ -1,22 +1,16 @@
-CREATE TABLE
-    public.daily (
-         id bigint NOT NULL DEFAULT unique_rowid(),
-         address character varying(41) NOT NULL,
-         earnings real NOT NULL DEFAULT 0.0,
-         date character varying(10) NOT NULL,
-         PRIMARY KEY (id)
+CREATE TABLE "public"."daily" (
+    "id" SERIAL PRIMARY KEY,
+    "address" VARCHAR(41) not null,
+    "earnings" REAL not null default 0,
+    "date" DATE not null default CURRENT_DATE
 );
+CREATE UNIQUE INDEX "daily_addr_date" on "public"."daily"("address", "date");
 
-ALTER TABLE public.daily
-    ADD CONSTRAINT daily_address_date_unique UNIQUE (address, date);
-
-CREATE TABLE
-    public.wallet (
-          id bigint NOT NULL DEFAULT unique_rowid(),
-          name character varying(24) NOT NULL,
-          address character varying(41) NOT NULL,
-          nodes smallint NOT NULL DEFAULT 0,
-          balance real NOT NULL DEFAULT 0.0,
-          daily real NOT NULL DEFAULT 0.0,
-          PRIMARY KEY (id)
+CREATE TABLE "public"."wallet" (
+      "id" SERIAL PRIMARY KEY,
+      "name" VARCHAR(24) not null default NOW(),
+      "address" VARCHAR(41) not null,
+      "nodes" INT2 not null default 0,
+      "balance" REAL not null default 0,
+      "daily" REAL not null default 0
 );
