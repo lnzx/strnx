781fdc339f55ec8df575d65761424f70eb1b887d
lnzx <lnx@tutamail.com>
2023-04-29 10:50:00 +0800

    DROP wallet name DEFAULT

--- db.sql
+++ db.sql
@@ -8,7 +8,7 @@ CREATE UNIQUE INDEX "daily_addr_date" on "public"."daily"("address", "date");
 
 CREATE TABLE "public"."wallet" (
       "id" SERIAL PRIMARY KEY,
-      "name" VARCHAR(24) not null default NOW(),
+      "name" VARCHAR(24) not null,
       "address" VARCHAR(41) not null,
       "nodes" INT2 not null default 0,
       "balance" REAL not null default 0,
