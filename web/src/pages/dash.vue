aa112c3a305c089f1f9106eb4635a5729ecbcb2a
lnzx <lnx@tutamail.com>
2023-05-31 16:11:51 +0800

    fix css

--- web/src/pages/dash.vue
+++ web/src/pages/dash.vue
@@ -1,22 +1,21 @@
 <route lang="yaml">
 meta:
-    requiresAuth: true
+  requiresAuth: true
 </route>
 
 <template>
-    <div style="height:95.8%">
-        <Nav />
-        <div class="columns" style="height:95.8%">
-          <Menu />
-          <!-- main section -->
-          <router-view /> 
-        </div>
+  <div>
+    <Nav />
+    <div class="columns">
+      <Menu />
+      <!-- main section -->
+      <router-view />
     </div>
+  </div>
 </template>
 
-
 <style scoped>
-
-.bg-color {background-color:#eef0f4;}
-
+.bg-color {
+  background-color: #eef0f4;
+}
 </style>
